8ce071609ae4add5bca11d0e9c850ba526d0847a
WouterVerschuren <finwou@gmail.com>
2023-06-08 12:33:35 +0200

    full stack

--- src/pages/AddLocation.js
+++ src/pages/AddLocation.js
@@ -3,57 +3,82 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function AddLocation() {
-    const [longtitude, setLongtitude] = useState(0);
-    const [latitude, setLatitude] = useState(0);
+    const [name, setName] = useState('');
+    const [longitude, setLongitude] = useState('');
+    const [latitude, setLatitude] = useState('');
     const [information, setInformation] = useState('');
-    const [rating, setRating] = useState(0);
-    const [checked, setChecked] = useState(false);
 
     const handleSubmit = (event) => {
         event.preventDefault();
+
         const data = {
-            Longtitude: longtitude,
+            Name: name,
+            Longitude: longitude,
             Latitude: latitude,
-            Infomation: information,
-            Rating: rating,
-            Checked: checked
+            Information: information,
         };
+
         axios.post('https://localhost:7149/api/Location', data)
-            .then(response => console.log(response.data))
-            .catch(error => console.error(error));
+            .then(response => {
+                console.log(response.data);
+                // Handle the response as needed
+            })
+            .catch(error => {
+                console.error(error);
+                // Handle the error as needed
+            });
+
+        console.log('Data:', data); // Log the data object
+
+
+
     };
 
     return (
         <div className='vakje'>
             test
-            <div className='form'>
+            <div className='form vakje2'>
                 <form onSubmit={handleSubmit}>
-                    <label>
-                        Longtitude:
-                        <input type="number" value={longtitude} onChange={(event) => setLongtitude(event.target.value)} />
-                    </label>
-                    <br />
-                    <label>
-                        Latitude:
-                        <input type="number" value={latitude} onChange={(event) => setLatitude(event.target.value)} />
-                    </label>
-                    <br />
-                    <label>
-                        Information:
-                        <input type="text" value={information} onChange={(event) => setInformation(event.target.value)} />
-                    </label>
-                    <br />
-                    <label>
-                        Rating:
-                        <input type="number" value={rating} onChange={(event) => setRating(event.target.value)} />
-                    </label>
-                    <br />
-                    <label>
-                        Checked:
-                        <input type="checkbox" checked={checked} onChange={(event) => setChecked(event.target.checked)} />
-                    </label>
-                    <br />
-                    <button type="submit">Submit</button>
+                    <ul className="form-style-1">
+                        <li>
+                            <label>Name of location <span className="required">*</span></label>
+                            <input
+                                type="text"
+                                value={name}
+                                onChange={(event) => setName(event.target.value)}
+                                className="field-divided"
+                                placeholder="name"
+                            />
+                        </li>
+                        <li>
+                            <label>Longitude and Latitude <span className="required">*</span></label>
+                            <input
+                                type="text"
+                                value={longitude}
+                                onChange={(event) => setLongitude(event.target.value)}
+                                className="field-divided"
+                                placeholder="Longitude"
+                            />
+                            <input
+                                type="text"
+                                value={latitude}
+                                onChange={(event) => setLatitude(event.target.value)}
+                                className="field-divided"
+                                placeholder="Latitude"
+                            />
+                        </li>
+                        <li>
+                            <label>I want this place for information about the location<span className="required">*</span></label>
+                            <textarea
+                                value={information}
+                                onChange={(event) => setInformation(event.target.value)}
+                                className="field-long field-textarea"
+                            ></textarea>
+                        </li>
+                        <li>
+                            <input type="submit" value="Submit" />
+                        </li>
+                    </ul>
                 </form>
             </div>
         </div>
