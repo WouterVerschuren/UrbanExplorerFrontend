bf11815427dbda48f4fa0f391994a0dbd9ac4063
WouterVerschuren <finwou@gmail.com>
2023-06-14 23:06:10 +0200

    crud and deploymenttest

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MapPage from "./pages/MapPage";
 import Sidebar from "./pages/Sidebar";
 import AddLocation from "./pages/AddLocation";
 import MyLocation from "./pages/MyLocations";
+import LocationCrud from "./pages/LocationCrud"; // Import the LocationCrud component from the correct file
 
 function App() {
   const [location, setLocation] = useState("map");
@@ -19,19 +20,9 @@ function App() {
     setLocation("mylocation");
   };
 
-
-  // const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'darkblue', 'purple'];
-  // let currentColorIndex = 0;
-
-  // setInterval(() => {
-  //   const transitionDuration = 1; // duration of the transition in seconds
-  //   const delay = 0.1; // delay before the transition starts in seconds
-  //   document.body.style.transition = `background-color ${transitionDuration}s ease-in-out ${delay}s`;
-  //   document.querySelector('.sidebar').style.transition = `background-color ${transitionDuration}s ease-in-out ${delay}s`;
-  //   document.body.style.backgroundColor = colors[currentColorIndex];
-  //   document.querySelector('.sidebar').style.backgroundColor = colors[currentColorIndex];
-  //   currentColorIndex = (currentColorIndex + 1) % colors.length;
-  // }, 1000); // total duration of each cycle in milliseconds
+  const handleLocationCrudClick = () => {
+    setLocation("locationCrud");
+  };
 
   return (
     <div className="parent">
@@ -40,12 +31,14 @@ function App() {
           handleLocationClick={handleLocationClick}
           handleBackToMapClick={handleBackToMapClick}
           handleMyLocationClick={handleMyLocationClick}
+          handleLocationCrudClick={handleLocationCrudClick} // Pass the new handler function
         />
       </div>
       <div className="child map">
         {location === "map" && <MapPage />}
         {location === "add" && <AddLocation />}
         {location === "mylocation" && <MyLocation />}
+        {location === "locationCrud" && <LocationCrud />}
       </div>
     </div>
   );
